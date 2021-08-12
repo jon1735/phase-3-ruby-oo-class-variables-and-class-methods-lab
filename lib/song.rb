19d3e52d94b60cca7e6cdd3dfedb5840df7cfa22
Jonathan Gonzalez <Jonathan.gonzalez@powerhrg.com>
2021-08-12 15:41:35 -0400

    adding updated song.rb file

--- lib/song.rb
+++ lib/song.rb
@@ -39,6 +39,18 @@ class Song
     end
         genre_count
   end
+
+  def self.artist_count
+    artist_count = {}
+    @@artists.each do |artist|
+      if artist_count.include?(artist)
+        artist_count[artist] += 1
+      else
+        artist_count[artist] = 1
+      end
+    end
+        artist_count
+  end
 end
 
 # need to return list of genres and the amount of songs in each of those genres. Rock => 1
